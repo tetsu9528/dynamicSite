2f31a9642d0e10a5d7425d85f8c1a8585e12d0fd
tetsu9528 <ret.kiy1947@gmail.com>
2024-05-23 17:34:46 +0900

    second commit

--- src/components/genre/slideShow.js
+++ src/components/genre/slideShow.js
@@ -1,31 +1,31 @@
-import React from "react";
-const SlideShow = () => {
+// import React from "react";
+// const SlideShow = () => {
    
-  return (
+//   return (
 
-    <div class="swiper">
+//   //   <div class="swiper">
     
-    <div class="swiper-wrapper">
+//   //   <div class="swiper-wrapper">
       
-      <div class="swiper-slide">
-        <img src="https://dummyimage.com/600x400/fff/000"/>
-      </div>
-      <div class="swiper-slide">
-        <img src="https://dummyimage.com/600x400/fff/000"/>
-      </div>
-      <div class="swiper-slide">
-        <img src="https://dummyimage.com/600x400/fff/000"/>
-      </div>
-    </div>
+//   //     <div class="swiper-slide">
+//   //       <img src="https://dummyimage.com/600x400/fff/000"/>
+//   //     </div>
+//   //     <div class="swiper-slide">
+//   //       <img src="https://dummyimage.com/600x400/fff/000"/>
+//   //     </div>
+//   //     <div class="swiper-slide">
+//   //       <img src="https://dummyimage.com/600x400/fff/000"/>
+//   //     </div>
+//   //   </div>
     
-    <div class="swiper-pagination"></div>
+//   //   <div class="swiper-pagination"></div>
    
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
-  </div>
+//   //   <div class="swiper-button-prev"></div>
+//   //   <div class="swiper-button-next"></div>
+//   // </div>
 
 
-  );
-};
+//   );
+// };
 
-export default SlideShow;
+// export default SlideShow;
